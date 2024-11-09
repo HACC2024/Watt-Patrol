c7260ab8e29cc11400ec561567a24df7ca7ab0a0
tiffanywilliams <tiffanynhiwilliams@gmail.com>
2024-11-09 02:10:22 -1000

    Moved messages to separate json & changed solarpanel to solar-panel

--- src/app/lightbulb/lightbulb.component.ts
+++ src/app/lightbulb/lightbulb.component.ts
@@ -1,4 +1,14 @@
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import * as lightbulbMessageAudio from './lightbulb-messages-audio.json';
+
+interface MessageItem {
+    msg: string;
+    file: string;
+}
+
+interface LightbulbMessageAudio {
+  [key: string]: MessageItem[];
+}
 
 @Component({
   selector: 'app-lightbulb',
@@ -10,58 +20,13 @@ export class LightbulbComponent implements OnInit, OnChanges {
 
   showMsg: boolean = false;
 
-  info = {
-    'ac': [
-      'Air conditioning unit for cooling indoor spaces.',
-      'Energy-efficient models can help reduce electricity bills.'
-    ],
-    'dryer': [
-      'Electric or gas-powered appliance for drying clothes.',
-      'Ensure lint filter is cleaned regularly for safety.'
-    ],
-    'ceiling-fan': [
-      'Helps circulate air and keep rooms cool.',
-      'Ceiling fans can be an energy-efficient cooling option.'
-    ],
-    'refrigerator': [
-      'Essential appliance for preserving food.',
-      'Check temperature settings for optimal food storage.'
-    ],
-    'light': [
-      'Provides illumination for indoor spaces.',
-      'Use LED bulbs for energy efficiency.'
-    ],
-    'oven': [
-      'Used for baking, roasting, and cooking food.',
-      'Check for preheat settings for best results.'
-    ],
-    'porch-light': [
-      'Enhances outdoor visibility and safety.',
-      'Consider motion-sensor lights for convenience.'
-    ],
-    'solarpanel': [
-      'Harness solar energy to power your home.',
-      'Can significantly reduce electricity costs.'
-    ],
-    'tv': [
-      'Provides entertainment and information.',
-      'Smart TVs can access streaming services.'
-    ],
-    'washer': [
-      'Cleans clothes using water and detergent.',
-      'Front-load washers are often more energy-efficient.'
-    ]
-  } as { [key: string]: string[] };  
+  private lightbulbMessageAudio: LightbulbMessageAudio = (lightbulbMessageAudio as any).default;
 
-  idleMsgs = [
-    'Turn on an appliance to see how much electricity it uses!', 
-    'You can tap the screen or use the switch!'
-  ];
-  
   msg = '';
 
   ngOnChanges(): void {
-    if (this.info[this.itemToggled.name]) this.showApplianceMsg();
+    // console.log("Testing", lightbulbMessageAudio)
+    if (this.lightbulbMessageAudio[this.itemToggled.name]) this.showApplianceMsg();
   }
 
   ngOnInit(): void {
@@ -76,12 +41,13 @@ export class LightbulbComponent implements OnInit, OnChanges {
   }
 
   showApplianceMsg() {
-    const itemInfo = this.info[this.itemToggled.name];
+    const itemMessagesAudios = this.lightbulbMessageAudio[this.itemToggled.name];
     const appearTime = 10 * 1000;  // Changes how many seconds the message appears for.
 
     /* Only change message if not currently showing. */
     if (!this.showMsg) {
-      this.msg = itemInfo[Math.floor(Math.random() * itemInfo.length)];
+      const select = Math.floor(Math.random() * itemMessagesAudios.length);
+      this.msg = itemMessagesAudios[select].msg;
       this.showMsg = true;
 
       setTimeout(() => {
@@ -92,10 +58,12 @@ export class LightbulbComponent implements OnInit, OnChanges {
 
   showIdleMsg() {
     const appearTime = 10 * 1000;  // Changes how many seconds the message appears for.
+    const idleMessages = this.lightbulbMessageAudio['idle'];
+    const select = Math.floor(Math.random() * idleMessages.length);
 
     /* Only change message if not currently showing. */
     if (!this.showMsg) {
-      this.msg = this.idleMsgs[Math.floor(Math.random() * this.idleMsgs.length)];
+      this.msg = idleMessages[select].msg;
       this.showMsg = true;
 
       setTimeout(() => {
