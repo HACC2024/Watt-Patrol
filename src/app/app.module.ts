10a2cdd3a6a0101901357ac97c0819dbf1d9f40a
tiffanywilliams <60216343+tiffanywilliams@users.noreply.github.com>
2024-11-10 23:36:44 -1000

    Removed fusion charts & added ng deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,6 @@ import { DayNightSliderComponent } from './day-night-slider/day-night-slider.com
 import { ApplianceChartComponent } from './appliance-chart/appliance-chart.component';
 import { LightbulbComponent } from './lightbulb/lightbulb.component';
 
-// Import FusionChartsModule and the FusionCharts library
-import { FusionChartsModule } from 'angular-fusioncharts';
-
-// Import FusionCharts and the necessary chart types
-import FusionCharts from 'fusioncharts';
-import Charts from 'fusioncharts/fusioncharts.charts';
-import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-import Widgets from 'fusioncharts/fusioncharts.widgets';
-
-// Initialize FusionCharts with chart modules and theme
-FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme, Widgets);
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,7 +22,6 @@ FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme, Widgets);
   ],
   imports: [
     BrowserModule,
-    FusionChartsModule, // Include the FusionCharts module
     NgxSliderModule,
     AppRoutingModule
   ],
